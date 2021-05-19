91f3c86579c0aa84a2f1eb305d623ff5d03d5abd
Waheguru <jassivarsha@gmail.com>
2021-05-19 07:00:54 +0530

    DSC

---
+++ app/src/main/java/com/example/myapplication/Splash.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+@Suppress("DEPRECATION")
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash)
+
+      //JASSI
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2500)
+    }
+}
